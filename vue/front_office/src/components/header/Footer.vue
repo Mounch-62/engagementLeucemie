05c0fbfc11ac8ee00eb5dc182ee5064440d3c542
FloVth <florianvauthier70@gmail.com>
2024-02-09 10:52:13 +0100

    creation des pages header footer

---
+++ vue/front_office/src/components/header/Footer.vue
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<template>
+    <footer>
+        <div class="leftContainer">
+            Logo Engagement Leucémie
+        </div>
+        <div class="rightContainer">
+            <div class="socialNetworksContainer">
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/engagementleucemie/" target="_blank">
+                            Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/engagementleucemie/" target="_blank">
+                            Instagram
+                        </a>
+                        
+                    </li>
+                </ul>
+            </div>
+            <div class="coordinatesContainer">
+                <p>Coordonnées :</p>
+                <div class="coordinatesContent">
+
+                </div>
+                <div class="menuContainer">
+
+                </div>
+            </div>
+            
+        </div>
+    </footer>
+</template>
